ea5070ce6461c2ca779371f40fb0f60fbdb58767
Sid <sid@pop-os.localdomain>
2022-10-03 00:44:09 +0530

    added adjacent clause

--- src/fuze/core.clj
+++ src/fuze/core.clj
@@ -14,6 +14,17 @@
     (assoc game-state :game-over true :end-state "You win!")
     game-state))
 
+(defn isAdj? [active-block inactive-block]
+  (or
+   (and
+    (= (first inactive-block) (first active-block))
+    (or (= (second inactive-block) (inc (second  active-block)))
+        (= (second inactive-block)  (inc (second active-block)))))
+   (and
+    (= (second  inactive-block) (second active-block))
+    (or (= (first inactive-block) (inc (first  active-block)))
+        (= (first inactive-block)  (dec (first active-block)))))))
+
 (defn update-active-blocks [game-state]
   (let [new-active-blocks
         (filter not-empty
@@ -23,19 +34,11 @@
                     conj (filter some?
                                  (map
                                   (fn [active-block]
-                                    (if
-                                     (or
-                                      (and
-                                       (= (first inactive-block) (first active-block))
-                                       (or (= (second inactive-block) (inc (second  active-block)))
-                                           (= (second inactive-block)  (inc (second active-block)))))
-                                      (and
-                                       (= (second  inactive-block) (second active-block))
-                                       (or (= (first inactive-block) (inc (first  active-block)))
-                                           (= (first inactive-block)  (dec (first active-block))))))
+                                    (if (isAdj? active-block inactive-block)
                                       inactive-block
                                       nil)) (:active-blocks game-state)))))
                  (:inactive-blocks game-state)))
+        ; This is not ideal
         new-inactive-blocks (mapcat
                              (fn [[x n]] (repeat n x))
                              (apply merge-with - (map frequencies [(:inactive-blocks game-state) new-active-blocks])))]
@@ -60,28 +63,27 @@
 (defn draw-board
   "Prints the board(non-lazy)"
   [game-config game-state]
-  (do
-    (print (str (char 27) "[2J")) ; clear screen
-    (print (str (char 27) "[;H")) ; move cursor to the top left corner of the screen
-    (doall
-     (for [x (range (:breadth game-config))
-           y (range (:length game-config))]
-       (cond
-         (some #(= % [x y]) (:active-blocks game-state))  (if  (= y (dec (:length game-config)))
-                                                            (println "[  *  ]")
-                                                            (print "[  *  ]"))
-         (some #(= % [x y]) (:inactive-blocks game-state))  (if  (= y (dec (:length game-config)))
-                                                              (println "[  @  ]")
-                                                              (print "[  @  ]"))
-         (some #(= % [x y]) (:activation-blocks game-state)) (if  (= y (dec (:length game-config)))
-                                                               (println "[  X  ]")
-                                                               (print "[  X  ]"))
-         (some #(= % [x y]) (:hyperactive-blocks game-state)) (if  (= y (dec (:length game-config)))
-                                                                (println "[  #  ]")
-                                                                (print "[  #  ]"))
-         :else (if (= y (dec (:length game-config)))
-                 (println "[     ]")
-                 (print "[     ]")))))))
+  (do (print (str (char 27) "[2J")) ; clear screen
+      (print (str (char 27) "[;H")) ; move cursor to the top left corner of the screen
+      (doall
+       (for [x (range (:breadth game-config))
+             y (range (:length game-config))]
+         (cond
+           (some #(= % [x y]) (:active-blocks game-state))  (if  (= y (dec (:length game-config)))
+                                                              (println "[  *  ]")
+                                                              (print "[  *  ]"))
+           (some #(= % [x y]) (:inactive-blocks game-state))  (if  (= y (dec (:length game-config)))
+                                                                (println "[  @  ]")
+                                                                (print "[  @  ]"))
+           (some #(= % [x y]) (:activation-blocks game-state)) (if  (= y (dec (:length game-config)))
+                                                                 (println "[  X  ]")
+                                                                 (print "[  X  ]"))
+           (some #(= % [x y]) (:hyperactive-blocks game-state)) (if  (= y (dec (:length game-config)))
+                                                                  (println "[  #  ]")
+                                                                  (print "[  #  ]"))
+           :else (if (= y (dec (:length game-config)))
+                   (println "[     ]")
+                   (print "[     ]")))))))
 
 (defn -main []
   (let [game-config {:length 9
